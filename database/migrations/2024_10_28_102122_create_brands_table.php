91807b05e2ced904e6d08793e335b2aa41a792c8
Akash Patel <akashpatel.coderkube@gmail.com>
2024-10-29 11:02:39 +0530

    first commit

---
+++ database/migrations/2024_10_28_102122_create_brands_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->bigInteger('order')->nullable()->index();
+            $table->tinyInteger('status')->default(1)->comment('0 : inactive, 1 : active')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
